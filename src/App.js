afb4c8aff8040cbd179134b9662c3273d9a735e4
Your Name <you@example.com>
2023-12-05 13:34:24 +0530

    basic todo app by using reactjs and bootstrap

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
-function App() {
+const TodoApp = () => {
+  const [todos, setTodos] = useState([]);
+  const [input, setInput] = useState('');
+
+  const addTodo = () => {
+    if (input.trim() !== '') {
+      setTodos([...todos, input]);
+      setInput('');
+    }
+  };
+
+  const removeTodo = (index) => {
+    const newTodos = [...todos];
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mt-5">
+      <div className="row">
+        <div className="col-md-6 offset-md-3">
+          <h2 className="mb-4">Todo App</h2>
+          <div className="input-group mb-3">
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Add a new todo"
+              value={input}
+              onChange={(e) => setInput(e.target.value)}
+            />
+            
+            <div className="input-group-append ">
+              <button className="btn btn-primary button-left" type="button" onClick={addTodo}>
+                Add
+              </button>
+            </div>
+          </div>
+          <ul className="list-group">
+            {todos.map((todo, index) => (
+              <li key={index} className="list-group-item d-flex justify-content-between align-items-center">
+                {todo}
+                <button className="btn btn-danger btn-sm" onClick={() => removeTodo(index)}>
+                  Remove
+                </button>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
-export default App;
+export default TodoApp;
